d9d689b8182e29fd74cc81e14c394261f4b9528f
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-06-07 17:46:07 -0700

    Adds fixed but of bad url triggering error

--- src/Components/Location/Location.js
+++ src/Components/Location/Location.js
@@ -3,19 +3,26 @@ import './Location.css'
 import { Link, Redirect } from 'react-router-dom'
 
 const Location = ({ destination, shouldGo }) => {
-		console.log(shouldGo)
-		const goHere = destination.location
-		const JSX = (
-		<div className='location'>
-			<h1>{goHere.name}</h1>
-			<p>{goHere.street}</p>
-			<p>{goHere.city}, {goHere.state}</p>
-			<Link to='/results'><button>Back</button></Link>
-		</div>)
-		return(
-			<div>
-			{shouldGo? JSX : <Redirect to='/' />}
-			</div>
-		)
-}
+	const goHere = destination ? destination.location : null
+	if (!shouldGo) {
+		return <Redirect to="/" />;
+	}
+	
+	return (
+		<div className="location">
+			{goHere && (
+				<>
+					<h1>{goHere.name}</h1>
+					<p>{goHere.street}</p>
+					<p>
+						{goHere.city}, {goHere.state}
+					</p>
+					<Link to="/results">
+						<button>Back</button>
+					</Link>
+				</>
+			)}
+		</div>
+	);
+};
 export default Location
